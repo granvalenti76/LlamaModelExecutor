2a53286a5eac06a6c432f69865dfb30f4d9488fa
Luca T <luca.trav@gmail.com>
2026-06-20 11:25:17 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LlamaModelExecutor",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "LlamaModelExecutor",
+            targets: ["LlamaModelExecutor"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "LlamaModelExecutor",
+            swiftSettings: [
+                .enableUpcomingFeature("ApproachableConcurrency"),
+            ],
+        ),
+
+    ],
+    swiftLanguageModes: [.v6]
+)
